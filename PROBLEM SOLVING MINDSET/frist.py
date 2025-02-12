0709af3ef9654bd43602d686534615da1d1deba1
PrashantSinghRajputOfficial <prashantsinghrajputofficial@gmal.com>
2025-02-13 02:20:54 +0530

    dynamic llist porblem solved

---
+++ PROBLEM SOLVING MINDSET/frist.py
@@ -0,0 +1,53 @@
+# we have two list
+
+#list frist
+list_1 = [1,2,3,4,5]
+print("list frist : ",list_1)
+
+#list second
+list_2 = ["a","b","c","d","e"]
+print("list second : ",list_2)
+
+# Frist problem : making a list using frist and second list in alternating order
+list_3 = []
+for l_1,l_2 in zip(list_1,list_2):
+    list_3.append(l_1)
+    list_3.append(l_2)
+print("making third list using 1 and second list : ",list_3)
+
+# Second problem : make a dynamic list using first and second list, there list charactor in key and integer is value
+list_4 = []
+for l_3,l_4 in zip(list_1,list_2):
+    list_4.append({l_4 : l_3})
+print("dynamic list making using frist and second list : ",list_4)
+
+# Third problem : make a dynamic list using third list, there list charactor in key and integer is value
+list_4_2 = []
+for i in range(0, len(list_3), 2):
+    value = list_3[i]
+    key = list_3[i+1]
+    list_4_2.append({key : value})
+print("dynamic list making using list third : ",list_4_2)
+
+# Fourth porblem : makin again third list using dynamic list
+new_list_3 = []
+for item in list_4:
+    for keyword, value in item.items() :
+        new_list_3.append(value)
+        new_list_3.append(keyword)
+print("remakin list 3 using dynamic list : ",new_list_3)
+
+# fifth porblem : makin again second list using dynamic list
+new_list_2 = []
+for item1 in list_4:
+    for keyword1, value1 in item1.items() :
+        new_list_2.append(keyword1)
+print("remakin list 2 using dynamic list : ",new_list_2)
+
+# sixth porblem : makin again frist list using dynamic list
+
+new_list_1 = []
+for item2 in list_4:
+    for keyword2, value2 in item2.items():
+        new_list_1.append(value2)
+print("remakin list 1 using dynamic list : ",new_list_1)
