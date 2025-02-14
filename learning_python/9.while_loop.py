6afab6aaa2ec28e8809b7ec892b03b726fdd55da
PrashantSinghRajputOfficial <prashantsinghrajputofficial@gmal.com>
2025-02-14 05:35:46 +0530

    i am making a code using while loop for cheking eligibleity and sthoring phone number

---
+++ learning_python/9.while_loop.py
@@ -0,0 +1,54 @@
+# i am making a code using while loop for cheking eligibleity and sthoring phone number 
+while True:
+    while True:  
+        try:
+            age = int(input("Enter your age in number for cheking yor are eligible for this verification : "))
+            break  
+        except ValueError:
+            print("Invalid input! Please enter your age correctly in numbers.")
+    if age>18 and age<120:
+     print("You are eligible : ")
+     break
+    elif age<18 and age>0:
+     print("you are not eligible 'sorry' ")
+     exit()
+    else:
+     print("This age is not possibe please write correct age")
+list_of_number_or_name = []
+fix = True
+while True :
+    while True:
+            qna1 = input("\nyou want to save any person phone number : ")
+            if qna1.lower() == "yes":
+                break
+            elif qna1.lower() == "no":
+                 print("Ok sir, stored phone number list is completed")
+                 fix = False
+                 break
+            else:
+                print("Please enter yes or no only")
+    if fix == False:
+        break
+    else:
+        print()
+    while fix:
+        name = input("person name : ")  
+        try:
+            num = int(input(f"Enter {name}'s Number : "))
+
+            break  
+        except ValueError:
+            print("Invalid input! Please enter phone number correctly in numbers.")
+
+        print(f"{name}'s number are saved")
+            
+    list_of_number_or_name.append(f"{name} : {num}")
+        
+print("your saved number are : ")
+print(list_of_number_or_name)
+if not list_of_number_or_name:
+    print("you dont give any number")
+else:
+    for value in list_of_number_or_name:
+        print(f"- {value}")
+    
--- test.py
+++ test.py
@@ -1,118 +1,124 @@
-# {
-#  "cells": [
-#   {
-#    "cell_type": "code",
-#    "execution_count": null,
-#    "metadata": {},
-#    "outputs": [
-#     {
-#      "name": "stdout",
-#      "output_type": "stream",
-#      "text": [
-#       "List a: [1, 2, 3, 4, 5]\n",
-#       "List b: ['A', 'B', 'C', 'D', 'E']\n",
-#       "List c: [1, 'A', 2, 'B', 3, 'C', 4, 'D', 5, 'E']\n",
-#       "Multi-Dimensional List using List c: [{'A': [1]}, {'B': [2]}, {'C': [3]}, {'D': [4]}, {'E': [5]}]\n",
-#       "New List c reverted back from multi-dimensional list: [1, 'A', 2, 'B', 3, 'C', 4, 'D', 5, 'E']\n",
-#       "New List a reverted back from new List c: [1, 2, 3, 4, 5]\n",
-#       "New List b reverted back from new List c: ['A', 'B', 'C', 'D', 'E']\n"
-#      ]
-#     }
-#    ],
-#    "source": [
-#     "a=[1,2,3,4,5]\n",
-#     "b=['A','B','C','D','E']\n",
-#     "c=[]\n",
-#     "\n",
-#     "for i in range (len(a)):\n",
-#     "    c.append(a[i])\n",
-#     "    c.append(b[i])\n",
-#     "\n",
-#     "print(f'List a: {a}\\nList b: {b}\\nList c: {c}')\n",
-#     "\n",
-#     "multi_dimensional_c=[{c[i+1]: [c[i]]}for i in range (0,len(c),2)]\n",
-#     "\n",
-#     "print(f'Multi-Dimensional List using List c: {multi_dimensional_c}')\n",
-#     "\n",
-#     "new_c=[]\n",
-#     "for elements in multi_dimensional_c:\n",
-#     "    for key, value in elements.items():\n",
-#     "        new_c.append(value[0])\n",
-#     "        new_c.append(key)\n",
-#     "\n",
-#     "print(f'New List c reverted back from multi-dimensional list: {new_c}')\n",
-#     "\n",
-#     "new_a=new_c[0::2]\n",
-#     "new_b=new_c[1::2]\n",
-#     "\n",
-#     "print(f'New List a reverted back from new List c: {new_a}\\nNew List b reverted back from new List c: {new_b}')"
-#    ]
-#   },
-#   {
-#    "cell_type": "code",
-#    "execution_count": 5,
-#    "metadata": {},
-#    "outputs": [
-#     {
-#      "name": "stdout",
-#      "output_type": "stream",
-#      "text": [
-#       "Name in reverse: aytidA\n"
-#      ]
-#     }
-#    ],
-#    "source": [
-#     "name='Aditya'\n",
-#     "name_reversed=name[::-1]\n",
-#     "print(f'Name in reverse: {name_reversed}')"
-#    ]
-#   }
-#  ],
-#  "metadata": {
-#   "kernelspec": {
-#    "display_name": "base",
-#    "language": "python",
-#    "name": "python3"
-#   },
-#   "language_info": {
-#    "codemirror_mode": {
-#     "name": "ipython",
-#     "version": 3
-#    },
-#    "file_extension": ".py",
-#    "mimetype": "text/x-python",
-#    "name": "python",
-#    "nbconvert_exporter": "python",
-#    "pygments_lexer": "ipython3",
-#    "version": "3.9.13"
-#   }
-#  },
-#  "nbformat": 4,
-#  "nbformat_minor": 2
-# }
+# # {
+# #  "cells": [
+# #   {
+# #    "cell_type": "code",
+# #    "execution_count": null,
+# #    "metadata": {},
+# #    "outputs": [
+# #     {
+# #      "name": "stdout",
+# #      "output_type": "stream",
+# #      "text": [
+# #       "List a: [1, 2, 3, 4, 5]\n",
+# #       "List b: ['A', 'B', 'C', 'D', 'E']\n",
+# #       "List c: [1, 'A', 2, 'B', 3, 'C', 4, 'D', 5, 'E']\n",
+# #       "Multi-Dimensional List using List c: [{'A': [1]}, {'B': [2]}, {'C': [3]}, {'D': [4]}, {'E': [5]}]\n",
+# #       "New List c reverted back from multi-dimensional list: [1, 'A', 2, 'B', 3, 'C', 4, 'D', 5, 'E']\n",
+# #       "New List a reverted back from new List c: [1, 2, 3, 4, 5]\n",
+# #       "New List b reverted back from new List c: ['A', 'B', 'C', 'D', 'E']\n"
+# #      ]
+# #     }
+# #    ],
+# #    "source": [
+# #     "a=[1,2,3,4,5]\n",
+# #     "b=['A','B','C','D','E']\n",
+# #     "c=[]\n",
+# #     "\n",
+# #     "for i in range (len(a)):\n",
+# #     "    c.append(a[i])\n",
+# #     "    c.append(b[i])\n",
+# #     "\n",
+# #     "print(f'List a: {a}\\nList b: {b}\\nList c: {c}')\n",
+# #     "\n",
+# #     "multi_dimensional_c=[{c[i+1]: [c[i]]}for i in range (0,len(c),2)]\n",
+# #     "\n",
+# #     "print(f'Multi-Dimensional List using List c: {multi_dimensional_c}')\n",
+# #     "\n",
+# #     "new_c=[]\n",
+# #     "for elements in multi_dimensional_c:\n",
+# #     "    for key, value in elements.items():\n",
+# #     "        new_c.append(value[0])\n",
+# #     "        new_c.append(key)\n",
+# #     "\n",
+# #     "print(f'New List c reverted back from multi-dimensional list: {new_c}')\n",
+# #     "\n",
+# #     "new_a=new_c[0::2]\n",
+# #     "new_b=new_c[1::2]\n",
+# #     "\n",
+# #     "print(f'New List a reverted back from new List c: {new_a}\\nNew List b reverted back from new List c: {new_b}')"
+# #    ]
+# #   },
+# #   {
+# #    "cell_type": "code",
+# #    "execution_count": 5,
+# #    "metadata": {},
+# #    "outputs": [
+# #     {
+# #      "name": "stdout",
+# #      "output_type": "stream",
+# #      "text": [
+# #       "Name in reverse: aytidA\n"
+# #      ]
+# #     }
+# #    ],
+# #    "source": [
+# #     "name='Aditya'\n",
+# #     "name_reversed=name[::-1]\n",
+# #     "print(f'Name in reverse: {name_reversed}')"
+# #    ]
+# #   }
+# #  ],
+# #  "metadata": {
+# #   "kernelspec": {
+# #    "display_name": "base",
+# #    "language": "python",
+# #    "name": "python3"
+# #   },
+# #   "language_info": {
+# #    "codemirror_mode": {
+# #     "name": "ipython",
+# #     "version": 3
+# #    },
+# #    "file_extension": ".py",
+# #    "mimetype": "text/x-python",
+# #    "name": "python",
+# #    "nbconvert_exporter": "python",
+# #    "pygments_lexer": "ipython3",
+# #    "version": "3.9.13"
+# #   }
+# #  },
+# #  "nbformat": 4,
+# #  "nbformat_minor": 2
+# # }
 
-# Creating a Fruits list
-fruits = [
-    "Apple",
-    "Orange",
-    "Mango",
-    "Banana",
-    "Papaya"
-]
+# # Creating a Fruits list
+# fruits = [
+#     "Apple",
+#     "Orange",
+#     "Mango",
+#     "Banana",
+#     "Papaya"
+# ]
 
-fruits2 = [
-    1,
-    2,
-    3,
-    4,
-    5
-]
+# fruits2 = [
+#     1,
+#     2,
+#     3,
+#     4,
+#     5
+# ]
 
-fruits3 = []
-# Get Fruit name in sequencial order using for loop
+# fruits3 = []
+# # Get Fruit name in sequencial order using for loop
 
-for index,value in enumerate(fruits,start=1):
-    for index2,value2 in enumerate(fruits2,start=1):
-        if (index == index2):
-            fruits3.append([value,value2])
-print(fruits3)
+# for index,value in enumerate(fruits,start=1):
+#     for index2,value2 in enumerate(fruits2,start=1):
+#         if (index == index2):
+#             fruits3.append([value,value2])
+# print(frui)
+my_list = [2,3,4]
+
+if not my_list:  
+    print("The list is empty!")  
+else:  
+    print("The list is not empty!")  
